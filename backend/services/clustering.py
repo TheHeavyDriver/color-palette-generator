6eb184ffdae169b2fdf55525ea2d13e2b40eaf69
TheHeavyDriver <samriddhibhatnagar8@gmail.com>
2026-03-07 02:30:14 +0530

    04: Sort colors by dominance

--- backend/services/clustering.py
+++ backend/services/clustering.py
@@ -2,8 +2,18 @@ from sklearn.cluster import KMeans
 import numpy as np
 from config import DEFAULT_CLUSTERS
 
+
 def extract_dominant_colors(pixels: np.ndarray):
     kmeans = KMeans(n_clusters=DEFAULT_CLUSTERS, n_init=10)
-    kmeans.fit(pixels)
-    colors = kmeans.cluster_centers_
-    return colors.astype(int)
+
+    labels = kmeans.fit_predict(pixels)
+
+    centers = kmeans.cluster_centers_
+
+    counts = np.bincount(labels)
+
+    sorted_indices = np.argsort(counts)[::-1]
+
+    sorted_colors = centers[sorted_indices]
+
+    return sorted_colors.astype(int)
